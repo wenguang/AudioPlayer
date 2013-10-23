cc4ce884e360a43eaee1c5f421b362b202e127fc
wenguang <ivorygx@126.com>
2013-10-23 11:57:37 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by wenguang pan on 10/23/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AudioPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by wenguang pan on 10/23/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
